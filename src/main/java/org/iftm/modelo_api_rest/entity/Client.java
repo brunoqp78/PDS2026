697cc19722bbf24ccf3bd7cb3443401377ff4909
brunoqp78 <brunoqp78@gmail.com>
2026-03-20 19:38:03 -0300

    Atualizando e completando todas as relações entre tabelas. Exemplo nxn (manyToMany)

--- src/main/java/org/iftm/modelo_api_rest/entity/Client.java
+++ src/main/java/org/iftm/modelo_api_rest/entity/Client.java
@@ -2,6 +2,8 @@ package org.iftm.modelo_api_rest.entity;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,52 +11,63 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
 //anotação que informa ao JPA que a classe Client deverá persistir os seus dados no banco de dados.
 @Entity
-//anotação que informa ao JPA o nome da tabela no banco de dados.
+// anotação que informa ao JPA o nome da tabela no banco de dados.
 @Table(name = "tb_cliente")
 public class Client implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    //anotação que informa ao JPA que o campo abaixo será a chave primária no banco de dados
+    // anotação que informa ao JPA que o campo abaixo será a chave primária no banco
+    // de dados
     @Id
-    //anotação que informa ao JPA a regra de geração da chave, neste caso auto-incremental
+    // anotação que informa ao JPA a regra de geração da chave, neste caso
+    // auto-incremental
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    //anotação que informa ao JPA as caracteristicas de cada campo
+    // anotação que informa ao JPA as caracteristicas de cada campo
     @Column(name = "nome", length = 100, nullable = false)
     private String name;
     @Column(name = "cpf", length = 14, nullable = false, unique = true)
     private String cpf;
     @Column(name = "salario", nullable = false)
     private Double income;
-    @Column(name = "data_aniversario", nullable = false)    
+    @Column(name = "data_aniversario", nullable = false)
     private Instant birthDate;
     @Column(name = "num_filhos", nullable = false)
     private Integer children;
 
     @OneToOne
-    @JoinColumn(name="fk_endereco", referencedColumnName="id")
+    @JoinColumn(name = "fk_endereco", referencedColumnName = "id")
     private Address address;
 
     @ManyToOne
-    @JoinColumn(name="fk_categoria", referencedColumnName="id")
+    @JoinColumn(name = "fk_categoria", referencedColumnName = "id")
     private Category category;
 
-    //anotação que indica ao JPA que o atributo não deve existir no banco de dados
+    @ManyToMany
+    @JoinTable(name = "tb_cliente_produto", // Nome da terceira tabela que será criada
+            joinColumns = @JoinColumn(name = "cliente_id"), // A coluna que aponta para o Cliente
+            inverseJoinColumns = @JoinColumn(name = "produto_id") // A coluna que aponta para o Produto
+    )
+    private Set<Product> products = new HashSet<>();
+
+    // anotação que indica ao JPA que o atributo não deve existir no banco de dados
     @Transient
     private int idade;
-    
+
     public Client() {
     }
-    
-    public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children, Address address, Category category) {
+
+    public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children, Address address,
+            Category category) {
         this.id = id;
         this.name = name;
         this.cpf = cpf;
@@ -64,43 +77,54 @@ public class Client implements Serializable {
         this.address = address;
         this.category = category;
     }
-    
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getCpf() {
         return cpf;
     }
+
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
+
     public Double getIncome() {
         return income;
     }
+
     public void setIncome(Double income) {
         this.income = income;
     }
+
     public Instant getBirthDate() {
         return birthDate;
     }
+
     public void setBirthDate(Instant birthDate) {
         this.birthDate = birthDate;
     }
+
     public Integer getChildren() {
         return children;
     }
+
     public void setChildren(Integer children) {
         this.children = children;
-    }    
+    }
 
     public Address getAddress() {
         return address;
@@ -108,7 +132,7 @@ public class Client implements Serializable {
 
     public void setAddress(Address address) {
         this.address = address;
-    }    
+    }
 
     public Category getCategory() {
         return category;
@@ -124,6 +148,4 @@ public class Client implements Serializable {
                 + birthDate + ", children=" + children + "]";
     }
 
-
-    
 }
---
+++ src/main/java/org/iftm/modelo_api_rest/entity/Product.java
@@ -0,0 +1,75 @@
+package org.iftm.modelo_api_rest.entity;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Product implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome")
+    private String name;
+    @Column(name = "preco")
+    private Double price;
+
+    // O lado "Fraco" do Relacionamento ManyToMany 🔗
+    @ManyToMany(mappedBy = "products")
+    private Set<Client> clients = new HashSet<>();
+
+    // Construtor Vazio
+    public Product() {
+    }
+
+    // Construtor Cheio (Sem a coleção de clientes!)
+    public Product(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Set<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(Set<Client> clients) {
+        this.clients = clients;
+    }
+    
+}
