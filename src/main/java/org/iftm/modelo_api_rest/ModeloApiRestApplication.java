1fb44904eb37156e665fae1f691f509e35c51ba2
brunoqp78 <brunoqp78@gmail.com>
2026-04-24 21:23:37 -0300

    Criando o método run para a service simples, sem regras.

--- src/main/java/org/iftm/modelo_api_rest/ModeloApiRestApplication.java
+++ src/main/java/org/iftm/modelo_api_rest/ModeloApiRestApplication.java
@@ -1,11 +1,13 @@
 package org.iftm.modelo_api_rest;
 
+import java.time.Instant;
 import java.util.List;
 
 import org.iftm.modelo_api_rest.entities.Address;
 import org.iftm.modelo_api_rest.entities.Client;
 import org.iftm.modelo_api_rest.repositories.AddressRepository;
 import org.iftm.modelo_api_rest.repositories.ClientRepository;
+import org.iftm.modelo_api_rest.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,10 +18,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class ModeloApiRestApplication implements CommandLineRunner {
 
     // --- INJEÇÃO DOS REPOSITORIES (Para testar nossas consultas) ---
+    //relacionado a atividade A2
     @Autowired
     private ClientRepository clientRepository;
     @Autowired
     private AddressRepository addressRepository;
+
+    //relacionado a atividade A3
+    @Autowired
+    private ClientService clientService;
 
     public static void main(String[] args) {
         SpringApplication.run(ModeloApiRestApplication.class, args);
@@ -30,7 +37,27 @@ public class ModeloApiRestApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         System.out.println("\n=================================================");
-        System.out.println("🚀 INICIANDO BATERIA DE TESTES DO SISTEMA");
+        System.out.println("🚀 Camada Service : TESTES DOS SERVIÇOS BÁSICOS");
+        System.out.println("=================================================\n");
+        Client client = new Client(null, "Bruno", "11111111111", 8000.00, Instant.now(), 2);
+        Client clienteSalvo = clientService.insert(client);
+        System.out.println("O novo cliente tem ID : " + clienteSalvo.getId());
+
+        System.out.println("\nBuscando todos clientes:");
+        List<Client> clientes = clientService.findAll();
+        for (Client c : clientes) {
+            System.out.println(" - " + c.getName() + " (R$ " + c.getIncome() + ")");
+        }         
+
+        System.out.println("\nBuscando todos clientes que contém da no nome:");
+        clientes = clientService.findByNameLike("da");
+        for (Client c : clientes) {
+            System.out.println(" - " + c.getName() + " (R$ " + c.getIncome() + ")");
+        }         
+
+
+        System.out.println("\n=================================================");
+        System.out.println("🚀 Camada Repository : INICIANDO BATERIA DE TESTES DO SISTEMA");
         System.out.println("=================================================\n");
 
         // -------------------------------------------------------------
